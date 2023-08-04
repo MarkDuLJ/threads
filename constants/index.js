1cb62b1ade8d25a50d4e279d65aac0cba9650be9
Mark Du <dulongjiebackup@gmail.com>
2023-08-04 15:39:58 -0400

    add constants folder and file

---
+++ constants/index.js
@@ -0,0 +1,44 @@
+export const sidebarLinks = [
+    {
+      imgURL: "/svgs/home.svg",
+      route: "/",
+      label: "Home",
+    },
+    {
+      imgURL: "/svgs/search.svg",
+      route: "/search",
+      label: "Search",
+    },
+    {
+      imgURL: "/svgs/heart.svg",
+      route: "/activity",
+      label: "Activity",
+    },
+    {
+      imgURL: "/svgs/create.svg",
+      route: "/create-thread",
+      label: "Create Thread",
+    },
+    {
+      imgURL: "/svgs/community.svg",
+      route: "/communities",
+      label: "Communities",
+    },
+    {
+      imgURL: "/svgs/user.svg",
+      route: "/profile",
+      label: "Profile",
+    },
+  ];
+  
+  export const profileTabs = [
+    { value: "threads", label: "Threads", icon: "/svgs/reply.svg" },
+    { value: "replies", label: "Replies", icon: "/svgs/members.svg" },
+    { value: "tagged", label: "Tagged", icon: "/svgs/tag.svg" },
+  ];
+  
+  export const communityTabs = [
+    { value: "threads", label: "Threads", icon: "/svgs/reply.svg" },
+    { value: "members", label: "Members", icon: "/svgs/members.svg" },
+    { value: "requests", label: "Requests", icon: "/svgs/request.svg" },
+  ];

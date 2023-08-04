a8257b3b728bcd22aabf1cdced68b327d02c4fc2
Mark Du <dulongjiebackup@gmail.com>
2023-08-04 15:29:50 -0400

    topbar done, show signed user and org

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image"
 import Link from "next/link"
+import { OrganizationSwitcher, SignedIn, SignOutButton } from "@clerk/nextjs";
 
-// import logo from "@/public/svgs/next.svg"
 
 function Topbar(){
     return (
@@ -10,6 +10,25 @@ function Topbar(){
                 <Image src="/svgs/edit.svg" alt="log" width={28} height={28} />
                 <p className="text-heading3-bold text-light-1 max-xs:hidden">Threds</p>
             </Link>
+
+            <div className='flex items-center gap-1'>
+                <div className='block md:hidden'>
+                <SignedIn>
+                    <SignOutButton>
+                    <div className='flex cursor-pointer'>
+                        <Image
+                        src='/svgs/logout.svg'
+                        alt='logout'
+                        width={24}
+                        height={24}
+                        />
+                    </div>
+                    </SignOutButton>
+                </SignedIn>
+
+                </div>
+                <OrganizationSwitcher  appearance={{elements:{organizationSwitcherTrigger:"py-2 px-4"}}}/>
+            </div>
         </nav>
     )
 }

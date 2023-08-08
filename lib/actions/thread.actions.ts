67edd34cd15aa18269d9691b240761159751ef52
Mark Du <dulongjiebackup@gmail.com>
2023-08-08 16:32:16 -0400

    show all posts in home page

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -33,3 +33,32 @@ export async function createThread({text,author,communityId,path}:Params) {
 
     
 }
+
+export async function fetchPosts(pageNumber=1, pageSize=20) {
+    connectToDB()
+
+    // cal how many posts to skip
+    const skipAmount = pageSize*(pageNumber-1)
+
+    //fetch the top level posts, which means no parents
+    const postsQuery = Thread.find({parentId:{$in:[null, undefined]}})
+                        .sort({createdAt:'desc'})
+                        .skip(skipAmount)
+                        .populate({path:"author", model:User})
+                        .populate({
+                            path:"children",
+                            populate:{
+                                path:"author",
+                                model:User,
+                                select:"_id name parentId image"
+                            }
+                        })
+
+    const totalPostCount = await Thread.countDocuments({parentId:{$in:[null, undefined]}})
+
+    const posts = await postsQuery.exec()
+
+    const isNext = totalPostCount > skipAmount +posts.length
+
+    return {posts,isNext}
+}

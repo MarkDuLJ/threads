b580621e6e72a8de384ebc93ffd91c41f32d3b86
Mark Du <dulongjiebackup@gmail.com>
2023-08-10 14:04:06 -0400

    show nested thread

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -35,6 +35,20 @@ async function Page({params}:{params:{id:string}}){
             <div className="mt-7">
                 <Comment  threadId={post.id} currentUserImg={user.imageUrl} currentUserId={JSON.stringify(userInfo._id)}/>
             </div>
+
+            <div className="mt-7">
+                {post.children.map((post:any)=>(
+                    <ThreadCard key={post._id} id={post._id} 
+                    currentUserId={user?.id || ""} 
+                    parentId={post.parentId} 
+                    content={post.text} 
+                    author={post.author}
+                    community={post.community}
+                    createdAt={post.createdAt}
+                    comments={post.children}
+                    isComment/>
+                ))}
+            </div>
         </section>
         
     )
--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -35,7 +35,7 @@ const ThreadCard =({id,
     comments,
     isComment}:Props)=>{
     return (
-        <article className="flex flex-col w-full rounded-x1 bg-dark-2 p-7">
+        <article className={`flex flex-col w-full rounded-x1  ${isComment ? "px-0 xs:px-7":"bg-dark-2 p-7"}`}>
             <div className="flex items-start justify-between">
                 <div className="flex w-full flex-1 flex-row gap-4">
                     <div className="flex flex-col items-center">
